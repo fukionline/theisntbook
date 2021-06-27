3c913d85cb011ba647acabfe35474f17e00ea874
pixdoet <63037136+pixdoet@users.noreply.github.com>
2021-06-27 17:05:04 +0800

    First and possibly only commit

---
+++ footer.php
@@ -0,0 +1,10 @@
+  <center>
+  <p><a href="about.php">about</a>&nbsp;&nbsp;
+  <a href="contact.php">contact</a>&nbsp;&nbsp;
+  <a href="faq.php">faq</a>&nbsp;&nbsp;
+  <a href="#">advertise</a>&nbsp;&nbsp;
+  <a href="terms.php">terms</a>&nbsp;&nbsp;
+  <a href="policy.php">privacy</a>
+  <br>a Fuck production
+  <br>Thefuckbook © 2005
+  </p></center>
